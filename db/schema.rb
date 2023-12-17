b90c124d15499b87883a9afea12007a02bfdb6ba
blainefreestone <blainefreestone01@gmail.com>
2023-12-17 11:09:25 -0800

    Lockable implemented.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_183032) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_190540) do
   create_table "leads", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
@@ -44,6 +44,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_183032) do
     t.boolean "student", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
