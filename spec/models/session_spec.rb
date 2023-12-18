9bcf74839ae55f8b029f258151d7d34bf7789681
blainefreestone <blainefreestone01@gmail.com>
2023-12-18 10:20:24 -0800

    Created session with spec and factory.

---
+++ spec/models/session_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Session, type: :model do
+  let (:session) { build(:session) }
+
+  context "with complete information" do
+    it "should save" do
+      expect(session.save).to be true
+    end
+  end
+end
