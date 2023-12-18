fd31781bf509c1ee05b84abdc09d728ba2916270
blainefreestone <blainefreestone01@gmail.com>
2023-12-17 22:53:58 -0800

    engaged_tutor and engaged_student added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,13 @@ class User < ApplicationRecord
   has_one :personal_information
   accepts_nested_attributes_for :personal_information
 
+  # Student engagement associations for users that are tutors.
   has_many :student_engagements, foreign_key: "tutor_id", class_name: "Engagement"
+  has_many :engaged_students, through: :student_engagements, source: :student
+
+  # Tutor engagement association for users that are students.
   has_one :tutor_engagement, foreign_key: "student_id", class_name: "Engagement"
+  has_one :engaged_tutor, through: :tutor_engagement, source: :tutor
 
   private
 
