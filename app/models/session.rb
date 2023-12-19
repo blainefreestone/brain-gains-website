93fc86aae97af61b6b449ddd9d230c048caa5424
blainefreestone <blainefreestone01@gmail.com>
2023-12-19 14:36:28 -0800

    Session validation updated.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,14 @@
 class Session < ApplicationRecord
+  validate :engagement_must_have_tutor
+
   belongs_to :engagement
 
   has_one :student, through: :engagement
   has_one :tutor, through: :engagement
+
+  def engagement_must_have_tutor
+    unless engagement.tutor
+      errors.add(:engagement, "must have a tutor")
+    end
+  end
 end
