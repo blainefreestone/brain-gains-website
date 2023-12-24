68213eb680ca6c5f18c9e2616515705fed7ebc3f
blainefreestone <blainefreestone01@gmail.com>
2023-12-23 20:29:36 -0800

    Application form created for basic questions.

--- app/controllers/tutor_applications_controller.rb
+++ app/controllers/tutor_applications_controller.rb
@@ -8,14 +8,35 @@ class TutorApplicationsController < ApplicationController
   end
 
   def create
+    @application = current_user.build_tutor_application(tutor_application_params)
+
+    if @application.save
+      redirect_to user_tutor_application_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @application = current_user.tutor_application
   end
 
   def update
+    @application = current_user.tutor_application
+
+    if @application.update(tutor_application_params)
+      redirect_to user_tutor_application_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tutor_application_params
+    params.require(:tutor_application).permit(:educational_history, :tutoring_skills, :resume)
   end
 end
--- app/models/tutor_application.rb
+++ app/models/tutor_application.rb
@@ -1,3 +1,5 @@
 class TutorApplication < ApplicationRecord
+  validates :educational_history, :tutoring_skills, presence: true
+  
   belongs_to :tutor, class_name: "User"
 end
