62a6c73799dd7a9eecff72233a539383f5b563d1
blainefreestone <blainefreestone01@gmail.com>
2024-01-02 23:26:03 -0800

    Prices are retrieved from student_offerings and passed to the view.

--- app/controllers/stripe/checkout_controller.rb
+++ app/controllers/stripe/checkout_controller.rb
@@ -1,5 +1,8 @@
 class Stripe::CheckoutController < ApplicationController
+  before_action :authenticate_user!
+  
   def pricing
-
+    offers = current_user.offers
+    @prices = offers.each_with_object([]) { |offer, prices| prices.push(Stripe::Price.retrieve(offer.stripe_price_id)) }
   end
 end
