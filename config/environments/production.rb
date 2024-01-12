20edda7465bb0a6372d06bfc5a891c773af356a6
blainefreestone <blainefreestone01@gmail.com>
2024-01-11 20:30:57 -0800

    Production smtp settings updated.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "braingainsidaho.com",
+    user_name: "apikey",
+    password: Rails.application.credentials.sendgrid[:secret]
+  }
+  config.action_mailer.default_url_options = {host: "braingainsidaho.com"}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
